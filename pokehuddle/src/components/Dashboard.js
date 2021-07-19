d64c697652a9fa9ae2c51dd70541efe60128ad19
Robert-Rodriguez <robertmasters@hotmail.com>
2021-07-19 01:31:50 -0400

    created CRA boilerplate, Login component, Dashboard component, and basic login functionality.

---
+++ pokehuddle/src/components/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import axios from 'axios';
+
+function dashboard() {
+
+
+
+  return (
+    <Router>
+        <div>hello</div>
+    </Router>
+  );
+}
+
+export default dashboard;
