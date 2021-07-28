ee2bf9ee5226afff9e46d8594d63d14fa3d0f71d
Robert-Rodriguez <robertmasters@hotmail.com>
2021-07-28 02:38:44 -0400

    .

--- pokehuddle/src/LandingPage.js
+++ pokehuddle/src/LandingPage.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 import PrivateRoute from './components/PrivateRoute';
 import Login from './components/LoginRegister/Login';
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/Dashboard/Dashboard';
 import Register from './components/LoginRegister/Register';
 import '../src/LandingPage.css'
 function LandingPage() {
@@ -18,7 +18,8 @@ function LandingPage() {
            */}
           <div className= "Route-container">
               <Switch>
-                <PrivateRoute exact path = '/dashboard' component = {Dashboard}></PrivateRoute>
+                {/* <PrivateRoute exact path = '/dashboard' component = {Dashboard}></PrivateRoute> */}
+                <Route path = '/test' component = {Dashboard} />
                 <Route path = '/login' component ={Login} />
                 <Route path = '/register' component ={Register} />
                 <Route component = {Login} />
