483fcdf5ee4947a73688c1c256ff59464515f984
Robert-Rodriguez <robertmasters@hotmail.com>
2021-07-29 04:54:42 -0400

    finished layout of dashboard landing page

--- pokehuddle/src/components/Pages/DashLanding.js
+++ pokehuddle/src/components/Pages/DashLanding.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import './DashLanding.css'
 
 export default function DashLanding() {
     return (
         <div className = "component-container">
-            Dash Landing
+            <h1>Welcome!</h1>
+            
+            <div className = "dash-landing-container">
+            
+                <div className = "favorite-notification-container">
+                    <div>
+                        <img className ="favoritestextimg" src = {require('../../images/favoritestext.png').default} alt = 'favorites' />
+                    </div>
+                    <div>
+                        <img className ="notificationstextimg" src = {require('../../images/notificationstext.png').default} alt = 'notifications' />
+                    </div>
+                </div>
+                
+                <div className = "news-container">
+                <img className ="newstextimg" src = {require('../../images/newstext.png').default} alt = 'news' />
+                </div>
+            </div>
         </div>
     )
 }
