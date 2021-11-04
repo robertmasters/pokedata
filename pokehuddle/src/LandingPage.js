5bc03c8b36323fffc36e2530489dea43598205d0
Robert-Rodriguez <robertmasters@hotmail.com>
2021-11-03 21:03:51 -0400

    uncommented privateRoute to dashboard

--- pokehuddle/src/LandingPage.js
+++ pokehuddle/src/LandingPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
 import axios from 'axios'
-
 import PrivateRoute from './components/PrivateRoute';
 import Login from './components/LoginRegister/Login';
 import Dashboard from './components/Dashboard/Dashboard';
@@ -11,13 +10,9 @@ function LandingPage() {
   return (
     <Router>
       <div className="main-container">
-            {/*          
-              <Link onClick={logout}>Logout</Link>
-              <Link to ='/dashboard'>dashboard</Link>
-           */}
           <div className="Route-container">
               <Switch>
-                {/* <PrivateRoute exact path = '/dashboard' component = {Dashboard}></PrivateRoute> */}
+                <PrivateRoute exact path = '/dashboard' component = {Dashboard}></PrivateRoute>
                 <Route exact path = '/' component = {Login} />
                 <Route path = '/test' component = {Dashboard} />
                 <Route path = '/login' component ={Login} />
