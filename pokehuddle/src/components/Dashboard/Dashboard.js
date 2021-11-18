5363dc98346f21ed5161d7f54d5626fecf7c1835
Robert-Rodriguez <robertmasters@hotmail.com>
2021-11-18 14:18:21 -0500

    added Header component to Dashboard and deleted css styling for old header image

--- pokehuddle/src/components/Dashboard/Dashboard.js
+++ pokehuddle/src/components/Dashboard/Dashboard.js
@@ -8,28 +8,27 @@ import PersonalNotes from '../Pages/PersonalNotes'
 import Photos from '../Pages/Photos'
 import Research from '../Pages/Research';
 import './DashboardStyles/Dashboard.css'
+import Header from '../LoginRegister/Header';
 
 function Dashboard() {
 
     return (
         <div className= "dash-container">
-            <div className= "banner-container">
-                <img className ="bannerimg" src = {require('../../images/PokeHuddle-Title.png').default} alt = 'Name Banner' />
-            </div> 
+            <Header />
 
             <div className= "center-content">
                 <div className= "sidebar">
                     <SideBar />
                 </div>
-            <Switch>
-                <Route path = '/test/home' component = {Home} />
-                <Route path = '/test/research' component = {Research} />
-                <Route path = '/test/favoritepokemon' component = {FavoritePokemon} />
-                <Route path = '/test/leaderboards' component = {Leaderboards} />
-                <Route path = '/test/personalnotes' component = {PersonalNotes} />
-                <Route path = '/test/photos' component = {Photos} />
-                <Route component = {Home} />
-            </Switch>
+                <Switch>
+                    <Route path = '/test/home' component = {Home} />
+                    <Route path = '/test/research' component = {Research} />
+                    <Route path = '/test/favoritepokemon' component = {FavoritePokemon} />
+                    <Route path = '/test/leaderboards' component = {Leaderboards} />
+                    <Route path = '/test/personalnotes' component = {PersonalNotes} />
+                    <Route path = '/test/photos' component = {Photos} />
+                    <Route component = {Home} />
+                </Switch>
             </div>
         </div>
     );
