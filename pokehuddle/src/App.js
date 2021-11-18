04410ea96a097c44d92a3eb261c2e0f68f5bfe8b
Robert-Rodriguez <robertmasters@hotmail.com>
2021-11-17 19:21:57 -0500

    created page not found component

--- pokehuddle/src/App.js
+++ pokehuddle/src/App.js
@@ -4,6 +4,7 @@ import PrivateRoute from './components/PrivateRoute';
 import Login from './components/LoginRegister/Login';
 import Dashboard from './components/Dashboard/Dashboard';
 import Register from './components/LoginRegister/Register';
+import PageNotFound from './components/Pages/PageNotFound';
 function App() {
 
   return (
@@ -15,7 +16,8 @@ function App() {
                 <Route exact path = '/' component = {Login} />
                 <Route path = '/test' component = {Dashboard} />
                 <Route path = '/login' component ={Login} />
-                <Route path = '/register' component ={Register} />  
+                <Route path = '/register' component ={Register} /> 
+                <Route component = {PageNotFound} /> 
               </Switch>
             <a target="_blank" className = "link" rel="noreferrer" href="https://github.com/robertmasters/pokehuddle/blob/main/Application_build_plan.md">Github  </a>with roadmap for app
           </div>
